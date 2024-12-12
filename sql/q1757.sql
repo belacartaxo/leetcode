9594c6fd76c19bd7c8c57fa3c7eae55f4a20b29e
Isabela Britto Cartaxo <belacartaxo@gmail.com>
2024-12-12 20:18:34 +0000

    new queries

---
+++ sql/q1757.sql
@@ -0,0 +1,9 @@
+'''
+1757. Recyclable and Low Fat Products
+Write a solution to find the ids of products that are both low fat and recyclable.
+
+Return the result table in any order.
+'''
+select product_id  
+from Products
+where low_fats='Y' and recyclable='Y'
---
+++ sql/q584.sql
@@ -0,0 +1,9 @@
+'''
+584. Find Customer Referee
+Find the names of the customer that are not referred by the customer with id = 2.
+
+Return the result table in any order.
+'''
+select name
+from Customer
+where referee_id <> 2 or referee_id is null
